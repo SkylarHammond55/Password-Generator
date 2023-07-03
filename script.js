9869d533ba6a6c30f6d2c5daf2bc0d5f521a385f
SkylarHammond55 <Skylarhammond55@gmail.com>
2023-07-03 18:08:59 -0500

    added password length

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Function to generate the password based on user prompts
+function generatePassword() {
+  var passwordLength = prompt("Enter the length of the password (between 8 and 128 characters):");
+
+  // Validate the password length
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length should be between 8 and 128 characters.");
+    return "";
+  }
+
+}
