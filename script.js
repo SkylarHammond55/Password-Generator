a7d3352f8d523f4dd1a9adb0e40e2b76c9bd1c03
SkylarHammond55 <Skylarhammond55@gmail.com>
2023-07-03 18:10:52 -0500

    added character type

--- script.js
+++ script.js
@@ -21,5 +21,36 @@ function generatePassword() {
     alert("Password length should be between 8 and 128 characters.");
     return "";
   }
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Validate that at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("At least one character type should be selected.");
+    return "";
+  }
+
+  // Define character pools based on selected criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
+
+  // Create the character pool based on selected criteria
+  var characterPool = "";
+  if (includeLowercase) {
+    characterPool += lowercaseChars;
+  }
+  if (includeUppercase) {
+    characterPool += uppercaseChars;
+  }
+  if (includeNumeric) {
+    characterPool += numericChars;
+  }
+  if (includeSpecial) {
+    characterPool += specialChars;
+  }
 
 }
